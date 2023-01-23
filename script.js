3c0a814988d535a2e87718944ce68047180111b4
PedroGomes2k <ph.gomes2k@gmail.com.br>
2023-01-23 13:11:43 -0300

    Colocar nome e e salvar

--- script.js
+++ script.js
@@ -1,31 +1,124 @@
-const nameUsers = [ ]
+let nameUsers = []
 
-const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nameUsers)
+let mensagem = []
 
-promise.then(respostaChegou())
-
-function respostaChegou(resposta){
-    console.log(resposta.data)
-}
 
 
 function nomeColocado() {
     // Vai ser perguntado o nome do usuario para ser salvo
     const nomeDoUsuario = prompt('Qual o seu nome')
+    if (nomeDoUsuario === '') {
+        alert('Por favor digite seu nome')
+        return nomeColocado()
+    } else {
+        nameUsers.push({ name: nomeDoUsuario })
+        console.log(nameUsers)
+    }
 
-   nameUsers.push(nomeDoUsuario)
-
+    //saveUsers()
 }
 nomeColocado()
 
-function mensagemEntrada() {
+function saveUsers() {
 
-    // Será feito a mensagem com o nome do usuario inserido nela
-    // Ultilizaçao do DOM para buscar qual inforçao necessária
-    const mensagem = document.querySelector('.mensagensEntrada')
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nameUsers)
 
-    mensagem.innerHTML= ` ${nameUsers} entra na sala... `
+    promise.then(respostaLogin)
+    promise.catch(erroLogin)
 
-    mensagem.classList.add('entrada')
 }
-mensagemEntrada()
+
+function respostaLogin() {
+
+    mostrarStatus()
+    setInterval(mostrarStatus, 5000)
+
+    mostrarMensagens()
+    setInterval(mostrarMensagens, 3000)
+
+    loadUsuarios()
+    setInterval(loadUsuarios, 10000)
+
+}
+
+function erroLogin(error) {
+
+    console.log(error.response)
+    if (error.response.status === 400) {
+        alert('Lamento, porém esse nome ja esta sendo usado. Por favor escolha outro!!')
+        window.location.reload()
+    }
+    if (error.response.status === 404) {
+        alert('Erro 404 Not Found... Tente novamente mais tarde')
+        windon.location.reload()
+    }
+}
+
+
+function mostrarStatus() {
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nameUsers)
+
+    promise.then(statusAtivado)
+    promise.catch(statusDesativado)
+}
+
+function statusAtivado() {
+    console.log('Usuario conectado')
+}
+
+function statusDesativado() {
+    console.log('Erro na conexão do Usuario!')
+}
+
+
+function mostrarMensagens() {
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    promise.then(mensagensEncontradas)
+    promise.catch(() => window.location.reload())
+
+}
+
+function loadUsuarios() {
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+    promise.then(loadEncontrado)
+    promise.catch(loadNaoEncontrado)
+}
+
+function loadEncontrado(){
+
+    usuario = response.data
+}
+
+function loadNaoEncontrado(){
+
+    alert('Usuários procurados nao encontrados')
+    window.location.reload()
+}
+
+
+function mensagensEncontradas(){
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function sideBarAbrir() {
+
+    const abrir = document.querySelector('.containerPrincipal .menu')
+    abrir.classList.toggle('escondido')
+    console.log(abrir)
+
+}
